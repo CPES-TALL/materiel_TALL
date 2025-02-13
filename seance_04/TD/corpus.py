ad3ce11582b2b4d13b93582323dc5ed66778c004
Pascal Amsili <Pascal.Amsili@ens.fr>
2025-02-13 09:21:29 +0100

    corrige partiel TP4

--- seance_04/TD/corpus.py
+++ seance_04/TD/corpus.py
@@ -17,3 +17,24 @@ for p in phrases:
 # Corpus : liste de phrases
 # Phrase(s) : liste de tokens
 # Token(s): string
+
+from collections import Counter, defaultdict
+
+def construit_modele(corpus, n=2):
+    modele = defaultdict(Counter)
+    for phrase in corpus:
+        phrase = ['<s>'] + phrase + ['</s>']
+        for i in range(len(phrase) - n + 1):
+            contexte, mot = tuple(phrase[i:i+n-1]), phrase[i+n-1]
+            modele[contexte][mot] += 1
+    return modele
+
+modele = construit_modele(corpus)
+
+# Exploitation du modèle
+# On n'est pas obligés de convertir en probas en fait: on prend le max.
+
+def prediction(m, contexte, n=2):
+    '''prédit le mot qui doit suivre en prenant
+    les derniers n-1 mots du contexte (si possible)'''
+    # à écrire
